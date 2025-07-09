4c24c1f995383be9eb97fe22ab500f522741afc5
Himanshu soni <himanshusoni@itgeeks.com>
2025-07-09 16:35:53 +0530

    add vercel realted files addti

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
 const { createRequestHandler } = require("@remix-run/vercel");
-const build = require("./build/server"); // Not ./build
+const build = require("./build/server/index.js"); 
 
 module.exports = createRequestHandler({ build });
