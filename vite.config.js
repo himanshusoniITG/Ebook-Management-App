dbe5fe4386155b27205d479d035e9d8cbaaeed0c
Himanshu soni <himanshusoni@itgeeks.com>
2025-07-09 20:44:40 +0530

    test

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,8 @@ import { vitePlugin as remix } from "@remix-run/dev";
 import { installGlobals } from "@remix-run/node";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { vercelPreset } from '@vercel/remix/vite';
 
 installGlobals({ nativeFetch: true });
-
- 
 
 // Related: https://github.com/remix-run/remix/issues/2835#issuecomment-1144102176
 // Replace the HOST env var with SHOPIFY_APP_URL so that it doesn't break the remix server. The CLI will eventually
@@ -55,7 +52,6 @@ export default defineConfig({
   },
   plugins: [
     remix({
-       presets: [vercelPreset()],
       ignoredRouteFiles: ["**/.*"],
       future: {
         v3_fetcherPersist: true,
