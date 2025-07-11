ecb69f4b7bcd5a6d8c5a680882de985d4e65312f
Himanshu soni <himanshusoni@itgeeks.com>
2025-07-11 17:53:35 +0530

    fix my code

--- app/routes/_index/route.jsx
+++ app/routes/_index/route.jsx
@@ -1,7 +1,7 @@
 import { redirect } from "@remix-run/node";
 import { Form, useLoaderData } from "@remix-run/react";
 import { login } from "../../shopify.server";
-import "./styles.css";
+import "./login.css";
 
 export const loader = async ({ request }) => {
   const url = new URL(request.url);
